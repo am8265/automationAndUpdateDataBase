d6d8f6f71a156b995be514ea09c1a5fd6b787f4a
ma2016 <mailme.ayan@gmail.com>
2023-12-13 15:42:43 -0500

    modify deinterleave

--- scripts/deinterleave_fastq.sh
+++ scripts/deinterleave_fastq.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Ayan Malakar
 # Usage: deinterleave_fastq.sh < interleaved.fastq f.fastq r.fastq [compress]
 # 
 # Deinterleaves a FASTQ file of paired reads into two FASTQ
@@ -8,8 +9,6 @@
 # Can deinterleave 100 million paired reads (200 million total
 # reads; a 43Gbyte file), in memory (/dev/shm), in 4m15s (255s)
 # 
-# Latest code: https://gist.github.com/3521724
-# Also see my interleaving script: https://gist.github.com/4544979
 # 
 # Inspired by Torsten Seemann's blog post:
 # http://thegenomefactory.blogspot.com.au/2012/05/cool-use-of-unix-paste-with-ngs.html
